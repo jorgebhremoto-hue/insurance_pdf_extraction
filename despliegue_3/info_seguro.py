fe66d1d4a0da89bad67f88782de80c2ade29d4db
Jorge Alfonso Bueno Herrera <jorgebhremoto@gmail.com>
2026-04-06 13:57:29 -0600

    Update info_seguro.py

--- despliegue_3/info_seguro.py
+++ despliegue_3/info_seguro.py
@@ -8,7 +8,7 @@ def extract_insurance_info(path):
     Vamos a hacer un ciclo for para navegar por todas las lineas del texto
     Dentro de ese ciclo for va a ver otro ciclo for donde vamos a recorrer todo el diccionario de posiciones
     Si el archivo no es una póliza de Chubb, entonces va a regresar None
-    En ese ciclo vamos a buscar si existe alguna de nuestras palabra clave y de existir, 
+    En ese ciclo vamos a buscar si existe alguna de nuestras palabra clave y de existir,
     vamos a tomar el número de linea dónde está la palabra
 
     En la siguiente parte vamos a procesar las lineas para extraer nuestra información deseada: Nombre, Póliza, Monto, Fecha límite de pago
@@ -17,33 +17,31 @@ def extract_insurance_info(path):
     """
 
     reader = PdfReader(path)
-    number_of_pages = len(reader.pages)  #para que usamos esto?
+    number_of_pages = len(reader.pages)  # para que usamos esto?
     page = reader.pages[0]
     text = page.extract_text()
     lines = text.split("\n")
 
-
-    posiciones_de_palabras = { 
-        "C.P." : None, 
-        "ASEGURADO": None,   # la ini y fin vigencia también está aquí
+    posiciones_de_palabras = {
+        "C.P.": None,
+        "ASEGURADO": None,  # la ini y fin vigencia también está aquí
         "AMIS": None,
         "PÓLIZA": None,
         "MONEDA": None,
         "PLAN": None,
-        "No tiene avisos de cobro.": None
-        }
-
+        "No tiene avisos de cobro.": None,
+    }
 
     for line_number, line in enumerate(lines):
-        #print(line)
+        # print(line)
         for palabra, posicion in posiciones_de_palabras.items():
             if palabra in line and posiciones_de_palabras[palabra] is None:
                 posiciones_de_palabras[palabra] = line_number
 
-    ###Condicional Qualitas Seguros 
+    ###Condicional Qualitas Seguros
 
-    #if posiciones_de_palabras["No tiene avisos de cobro."] == None:
-        #return None
+    if posiciones_de_palabras["No tiene avisos de cobro."] != None:
+        return None
 
     ####Nombre
     nombre = lines[posiciones_de_palabras["C.P."] + 1]
@@ -51,11 +49,11 @@ def extract_insurance_info(path):
     ###Monto
     monto = lines[posiciones_de_palabras["ASEGURADO"] - 1]
 
-    #procesando el vehículo
+    # procesando el vehículo
     existencia_vehiculo = lines[posiciones_de_palabras["AMIS"]]
     separar_para_auto = lines[posiciones_de_palabras["AMIS"]].split("AMIS")
     if existencia_vehiculo == None:
-        auto = '***'
+        auto = "***"
     else:
         cadena_auto_original = separar_para_auto[0].strip()
         partes = cadena_auto_original.split(")")
@@ -67,18 +65,18 @@ def extract_insurance_info(path):
     total_recibos = separar_recibos_cadena[1].lstrip()
     recibo_actual = separar_recibos_cadena[0]
 
-    if total_recibos == '04':
-        formapago = 'Trimestral'
-    elif total_recibos == '02':
-        formapago = 'Semestral'
-    elif total_recibos == '12':
-        formapago = 'Mensual'
-    elif total_recibos == '01':
-        formapago = 'contado'
+    if total_recibos == "04":
+        formapago = "Trimestral"
+    elif total_recibos == "02":
+        formapago = "Semestral"
+    elif total_recibos == "12":
+        formapago = "Mensual"
+    elif total_recibos == "01":
+        formapago = "contado"
     else:
-        formapago = '*'
+        formapago = "*"
 
-    #procesando póliza, endoso y fecha límite de pago
+    # procesando póliza, endoso y fecha límite de pago
     poliza_endoso_vencimiento_posicion = posiciones_de_palabras["PÓLIZA"]
     separar_datos_cadena = lines[poliza_endoso_vencimiento_posicion].split(" ")
     separar_dato_poliza = separar_datos_cadena[6].split("CONTROL")
@@ -87,25 +85,25 @@ def extract_insurance_info(path):
     endoso = separar_datos_cadena[7]
     flimite = separar_datos_cadena[8]
 
-    #procesando Inicio y Fin de Vigencia
+    # procesando Inicio y Fin de Vigencia
     ancla_vigencia_posicion = posiciones_de_palabras["PLAN"]
     fecha_ini_vigencia = lines[ancla_vigencia_posicion + 1].lstrip()
     fecha_fin_vigencia = lines[ancla_vigencia_posicion + 2].lstrip()
 
     print("termino el debug")
 
-    return{
+    return {
         "Nombre": nombre,
-        "Monto" : monto,
-        "Auto" : auto,
-        "Serie" : recibo_actual,
-        "Seriea" : total_recibos,
-        "Póliza" : poliza,
-        "Endoso" : endoso,
-        "Fecha Límite" : flimite,
-        "Forma de pago" : formapago,
-        "Inicio de Vigencia" : fecha_ini_vigencia,
-        "Fin de Vigencia" : fecha_fin_vigencia
+        "Monto": monto,
+        "Auto": auto,
+        "Serie": recibo_actual,
+        "Seriea": total_recibos,
+        "Póliza": poliza,
+        "Endoso": endoso,
+        "Fecha Límite": flimite,
+        "Forma de pago": formapago,
+        "Inicio de Vigencia": fecha_ini_vigencia,
+        "Fin de Vigencia": fecha_fin_vigencia,
     }
 
 
